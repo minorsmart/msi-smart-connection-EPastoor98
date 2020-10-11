8e9f94b8a64e09f64fb05588eeca51f2f71217ac
EPastoor98 <70443021+EPastoor98@users.noreply.github.com>
2020-10-11 12:49:03 +0200

    start

--- portfolio/pf.js
+++ portfolio/pf.js
@@ -1,12 +1,11 @@
-<script>
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
+if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar2").style.top = "0";
+} else {
+    document.getElementById("navbar2").style.top = "-80px";
 }
-</script>
+prevScrollpos = currentScrollPos;
+}
+
